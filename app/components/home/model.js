eaf15242d907240b96f6d5c48e228e8fbcdc7f61
Jovica Zaric <jovica.zaric@gmail.com>
2017-11-08 23:26:16 +0100

    Extended

--- app/components/home/model.js
+++ app/components/home/model.js
@@ -8,8 +8,8 @@ export const Product = t.struct({
 });
 
 export const products = [
-  {"id": 1, "name": "Huawei", "description": "p9", "price": 350, "discountValue": 0},
-  {"id": 2, "name": "Samsung", "description": "s8", "price": 400, "discountValue": 0},
-  {"id": 3, "name": "HTC", "description": "one m2", "price": 300, "discountValue": 0},
-  {"id": 4, "name": "Nokia", "description": "lumia 1025", "price": 450, "discountValue": 0}
+  {"id": 1, "name": "Huawei", "description": "p9", "price": 350, "discountValue": 0, "quantity": 2, "cost": 700},
+  {"id": 2, "name": "Samsung", "description": "s8", "price": 400, "discountValue": 0, "quantity": 0, "cost": 0},
+  {"id": 3, "name": "HTC", "description": "one m2", "price": 300, "discountValue": 0, "quantity": 0, "cost": 0},
+  {"id": 4, "name": "Nokia", "description": "lumia 1025", "price": 450, "discountValue": 0, "quantity": 0, "cost": 0}
 ];
